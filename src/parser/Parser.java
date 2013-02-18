503438d202bdf6dea302442a70eed49326d7a8b6
Abdurrosyid Handoyo <arrosyidbh@gmail.com>
2013-02-18 13:10:38 +0700

    initial commit, add file to be parsed

---
+++ src/parser/Parser.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parser;
+
+/**
+ *
+ * @author Yulianti Oenang
+ */
+public class Parser {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
